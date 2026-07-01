fc50252335563e9feb8e2b7a1e591c5e7d331170
Juan <jmbonyo08@gmail.com>
2026-06-30 21:55:04 -0300

    Ejercicios de Programacion Orientada a Objetos

---
+++ ClasesyObjetos 1/ClasesyObjetos1/ClasesyObjetos1.cs
@@ -0,0 +1,51 @@
+﻿namespace ClasesyObjetos1
+{
+    public class Perro
+    {
+        private string nombre;
+        private string raza;
+        private string altura;
+
+        public string Nombre
+        { 
+            get {return nombre;} 
+            set {nombre = value;}
+        }
+
+        public string Raza
+        {
+            get { return raza; }
+            set { raza = value; }
+        }
+
+        public string Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public Perro() { }
+
+        public Perro(string nombre, string raza, string altura)
+        { 
+            this.nombre = nombre;
+            this.raza = raza;  
+            this.altura = altura;
+        }
+
+        public void Dormir()
+        {
+            Console.WriteLine($"{nombre} esta durmiendo... Zzz");
+        }
+
+        public void Ladrar()
+        {
+            Console.WriteLine($"{nombre} dice: ¡Guau! ¡Guau!");
+        }
+
+        public string Comer(string carne)
+        {
+            return $"{nombre} mide {altura} y comera: {carne}";
+        }
+    }
+}
---
+++ ClasesyObjetos 1/ClasesyObjetos1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClasesyObjetos1;
+
+Perro perro1 = new Perro();
+perro1.nombre = "Negro";
+perro1.raza = "Labrador";
+perro1.altura = "60cm";
+
+Perro perro2 = new Perro("Luna", "Poodle", "30cm");
+
+Console.WriteLine("--- Perro 1 ---");
+perro1.Dormir();
+string resultadoComer = perro1.Comer("Hamburguesa");
+Console.WriteLine(resultadoComer);
+
+Console.WriteLine("\n--- Perro 2 ---");
+perro2.Ladrar();
+Console.WriteLine(perro2.Comer("Pollo"));
+
+Console.WriteLine("\nPresione cualquier tecla para salir...");
+Console.ReadKey();
